969269992efef0f8180ed963ac17ad201d270809
emrekaraomeroglu@gmail.com <emrekaraomeroglu@gmail.com>
2024-02-05 14:31:05 +0300

    Add project files.

---
+++ Core/UdemyCarBook.Domain/Entities/CarPricing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Domain.Entities
+{
+    public class CarPricing
+    {
+        public int CarPiricingID { get; set; }
+        public int CarID { get; set; }
+        public Car Car { get; set; }
+        public int PiricingID { get; set; }
+        public Pricing Pricing { get; set; }
+        public decimal Amount { get; set; }
+
+    }
+}
