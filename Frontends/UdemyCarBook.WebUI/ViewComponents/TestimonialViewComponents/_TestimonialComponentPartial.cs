2c248805e62aeddeb86ba2e8354c1c588f10bcf6
emrekaraomeroglu@gmail.com <emrekaraomeroglu@gmail.com>
2024-02-09 02:25:08 +0300

    frontend added UIPage created

---
+++ Frontends/UdemyCarBook.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdmyCarBook.Dto.AboutDtos;
+using UdmyCarBook.Dto.TestimonailDtos;
+
+namespace UdemyCarBook.WebUI.ViewComponents.TestimonialViewComponents
+{
+    public class _TestimonialComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var ResponseMessage = await client.GetAsync("https://localhost:7292/api/Testimonials");
+            if (ResponseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await ResponseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonailDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
