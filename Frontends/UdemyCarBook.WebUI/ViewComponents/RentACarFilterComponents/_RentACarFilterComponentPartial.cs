8cb7d400e5e1a5e896c33c3e2df7df4a42cb3a65
emrekaraomeroglu@gmail.com <emrekaraomeroglu@gmail.com>
2024-03-07 20:06:01 +0300

    create dropdown list

--- Frontends/UdemyCarBook.WebUI/ViewComponents/RentACarFilterComponents/_RentACarFilterComponentPartial.cs
+++ Frontends/UdemyCarBook.WebUI/ViewComponents/RentACarFilterComponents/_RentACarFilterComponentPartial.cs
@@ -4,9 +4,11 @@ namespace UdemyCarBook.WebUI.ViewComponents.RentACarFilterComponents
 {
     public class _RentACarFilterComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
-        {
-            return View();
-        }
-    }
+		public IViewComponentResult Invoke(string v)
+		{
+			v = "aaaa";
+			TempData["value"] = v;
+			return View();
+		}
+	}
 }
